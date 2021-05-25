4025d9e21bb03fa6123a77c4939e3219d2037fee
hasnahoque <999hasnahoque@gmail.com>
2021-05-25 10:43:43 +0900

    product page done

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css';
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0,10);
+    const[products, setProducts] = useState(first10);
+    
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                    {
+                        products.map(pd => <Product product ={pd}></Product>)
+                    }
+            </div> 
+            <div className="cart-container">
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Shop;
